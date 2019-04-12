786691dc2fe0d8708b64f78c6ddf330ad8f755ef
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-12 14:48:26 +0200

    add __author__ and __all__ parameters in __init__.py

--- screamshot/__init__.py
+++ screamshot/__init__.py
@@ -2,4 +2,12 @@
 Python library to capture screenshots of web applications
 """
 
+
+__author__ = """Maxime Courtet & Felix C"""
 __version__ = "0.0.1"
+
+
+from screamshot.core import ScreenShot
+
+
+__all__ = ['ScreenShot']
