e864756b01af998174c3a695f576b481798b743c
Félix Cloup <felixcloup@gmail.com>
2019-04-12 16:17:03 +0200

    fix setViewport

--- screamshot/core.py
+++ screamshot/core.py
@@ -1,24 +1,30 @@
 from pyppeteer import launch
+from operator import xor
 
+
+# If fullPage is set to True, width and height cannot reduce the size of the screenshot
 
 class ScreenShot(object):
 
-    def __init__(self, url, method, width=None, height=None, im_type='png',
+    def __init__(self, url, width=None, height=None, im_type='png',
                  selector=None, wait_for=None, fully_charged=False, render=False, data=None):
         assert isinstance(url, str), 'url parameter must be a string'
         self.url = url
 
-        assert (method == 'POST' or method == 'GET'), 'method must be equal to POST or GET'
-        self.method = method
-
-        if width:
-            assert (isinstance(width, int) and width >= 0), 'width must be a positive integer'
-            self.width = width
-
-        if height:
-            assert (isinstance(height, int) and height >= 0), 'height must be a positive integer'
-            self.height = height
-
+		if xor(height, width):
+			if width:
+				assert (isinstance(width, int) and width >= 0), 'width must be a positive integer'
+				self.argViewport = {'width': width, 'height': 600}
+			if height:
+				assert (isinstance(height, int) and height >= 0), 'height must be a positive integer'
+				self.argViewport = {'width': 800, 'height': height}
+		elif height and width:
+			assert (isinstance(width, int) and width >= 0), 'width must be a positive integer'
+			assert (isinstance(height, int) and height >= 0), 'height must be a positive integer'
+			self.argViewport = {'width': width, 'height': height}
+		else:
+			self.argViewport = None
+			
         assert (im_type == 'png' or im_type == 'jpeg'), 'im_type must be equal to png or jpeg'
         self.im_type = im_type
 
@@ -39,16 +45,11 @@ class ScreenShot(object):
         if data:
             assert isinstance(data, str), 'data must be a string'
         self.data = data
-    
+
     async def _init_page(self, browser):
         page = await browser.newPage()
-
-        page_params = dict()
-        if self.width:
-            page_params.update({'width': self.width})
-        if self.height:
-            page_params.update({'height': self.height})
-        page.setViewport(page_params)
+		if self.argViewport:
+   	    	await page.setViewport(page_params)
 
         await page.goto(self.url)
 
