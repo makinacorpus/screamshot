81ae6250704951ee4d206c460d98d5c23ccb3282
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-15 14:12:52 +0200

    bad url test

--- tests/SimpleScreamshotTests.py
+++ tests/SimpleScreamshotTests.py
@@ -5,12 +5,15 @@ from screamshot import ScreenShot
 
 class SimpleScreamshotTests(unittest.TestCase):
     def setUp(self):
-        self.screenShot = ScreenShot('https://www.google.fr')
+        self.base = ScreenShot('https://www.google.fr')
 
-    def test_works(self):
-        img = self.screenShot.load_and_screamshot()
+    def test_base(self):
+        img = self.base.load_and_screamshot()
         self.assertTrue(isinstance(img, bytes))
 
+    def test_bad_url(self):
+        with self.assertRaises(AssertionError):
+            ScreenShot(1)
 
 if __name__ == '__main__':
     unittest.main()
