82fedfda0d22945f7c148d4a6369b5fc5574a912
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-16 17:38:10 +0200

    No more sync function Add generate_bytes_img_prom

--- screamshot/core.py
+++ screamshot/core.py
@@ -93,7 +93,54 @@ async def _selector_manager(page, params):
     return page
 
 
-async def _generate_bytes_img(url, check_params, **kwargs):
+async def generate_bytes_img(url, **kwargs):
+    """
+    This function takes a screenshot and returns it as a `bytes` object
+
+    :param url: mandatory, the website's url
+    :type url: str
+
+    :param width: optionnal, the window's width
+    :type width: int
+
+    :param height: optionnal, the window's height
+    :type height: int
+
+    :param selector: optionnal, CSS3 selector, item whose screenshot is taken
+    :type selector: str
+
+    :param wait_for: optionnal, CSS3 selector, item to wait before taking the screenshot
+    :type wait_for: str
+
+    :param wait_until: optionnal, define how long you wait for the page to be loaded should \
+        be either load, domcontentloaded, networkidle0 or networkidle2
+    :type wait_until: str or list(str)
+
+    :returns: the base64 code of the image
+    :retype: `bytes`
+
+    .. warning:: It uses **pyppeteer** and so **async** functions
+
+    .. note :: The `asyncio` library can be used to manipulate this function
+
+    :Exemple:
+
+    .. code-block:: python
+
+        import asyncio
+
+        from screamshot import generate_bytes_img
+
+
+        async def main():
+            img = await generate_bytes_img('https://makina-corpus.com/expertise/cartographie',
+                                    selector='.image-right', wait_until='networkidle0')
+            print(img)
+
+
+        if __name__ == '__main__':
+            asyncio.get_event_loop().run_until_complete(main())
+    """
     params = _parse_parameters(**kwargs)
     if check_params:
         _check_params(url, params)
@@ -111,7 +158,7 @@ async def _generate_bytes_img(url, check_params, **kwargs):
     return image
 
 
-def generate_bytes_img(url, check_params=True, **kwargs):
+async def generate_bytes_img_prom(url, future, **kwargs):
     """
     This function takes a screenshot
 
@@ -158,5 +205,5 @@ def generate_bytes_img(url, check_params=True, **kwargs):
         if __name__ == '__main__':
             main()
     """
-    return asyncio.get_event_loop().run_until_complete(_generate_bytes_img(url, check_params,
-                                                                           **kwargs))
+    img = await generate_bytes_img(url, **kwargs)
+    future.set_result(img)
