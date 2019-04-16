8023952084f822dfa68be09eaaaabe01f2158280
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-16 17:45:30 +0200

    Add generate_bytes_img_prom to __all__ variable

--- screamshot/__init__.py
+++ screamshot/__init__.py
@@ -7,7 +7,7 @@ __author__ = """Maxime Courtet & Félix Cloup"""
 __version__ = "0.1.0"
 
 
-from screamshot.core import generate_bytes_img
+from screamshot.core import generate_bytes_img, generate_bytes_img_prom
 
 
-__all__ = ['generate_bytes_img']
+__all__ = ['generate_bytes_img', 'generate_bytes_img_prom']
