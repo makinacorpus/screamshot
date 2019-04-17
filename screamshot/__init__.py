6dea3afb6ef0c0bef4ca89ed4d12eab8112a55ce
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-17 11:19:50 +0200

    core.py -> generate_bytes_img.py

--- screamshot/__init__.py
+++ screamshot/__init__.py
@@ -7,7 +7,7 @@ __author__ = """Maxime Courtet & Félix Cloup"""
 __version__ = "0.1.1"
 
 
-from screamshot.core import generate_bytes_img, generate_bytes_img_prom
+from screamshot.generate_bytes_img import generate_bytes_img, generate_bytes_img_prom
 
 
 __all__ = ['generate_bytes_img', 'generate_bytes_img_prom']
