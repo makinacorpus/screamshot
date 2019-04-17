984bf91d1c7c6ea0c6bd23f2ea8bc990cf95176e
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-17 11:18:41 +0200

    update version parameter in the __init__file

--- screamshot/__init__.py
+++ screamshot/__init__.py
@@ -4,7 +4,7 @@ Python library to capture screenshots of web applications
 
 
 __author__ = """Maxime Courtet & Félix Cloup"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 from screamshot.core import generate_bytes_img, generate_bytes_img_prom
