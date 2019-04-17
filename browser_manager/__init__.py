df233367abc829f79c0ec6b900fed5bcdc4d3076
Félix Cloup <felixcloup@gmail.com>
2019-04-17 16:08:45 +0200

    Set the code as a usable library

---
+++ browser_manager/__init__.py
@@ -0,0 +1,4 @@
+from browser_manager.browser_manager_lib import goto_page, goto_page_async, get_browser, get_browser_list
+
+
+__all__ = ['goto_page', 'goto_page_async', 'get_browser', 'get_browser_list']
