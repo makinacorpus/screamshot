0fa58a1b5db4e8b91ca4ee7d69843986871694eb
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-23 15:14:52 +0200

    Update __init__ file

--- screamshot/__init__.py
+++ screamshot/__init__.py
@@ -7,9 +7,7 @@ __author__ = """Maxime Courtet & Félix Cloup"""
 __version__ = "0.1.2"
 
 
-from screamshot.utils import to_sync, get_browser, get_browser_sync, goto_page, goto_page_sync
 from screamshot.generate_bytes_img import generate_bytes_img, generate_bytes_img_prom
 
 
-__all__ = ['generate_bytes_img', 'generate_bytes_img_prom', "to_sync",
-           "get_browser", "get_browser_sync", "goto_page", "goto_page_sync"]
+__all__ = ['generate_bytes_img', 'generate_bytes_img_prom']
