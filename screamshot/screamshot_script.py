b643b97c278a6696c8e37b27d2cdfa4d0e8e6e02
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-23 15:15:50 +0200

    Update docstring in screamshot_script

--- screamshot/screamshot_script.py
+++ screamshot/screamshot_script.py
@@ -1,3 +1,6 @@
+"""
+Take a screenshot
+"""
 from argparse import ArgumentParser
 
 from screamshot import generate_bytes_img
@@ -28,48 +31,37 @@ async def do_it(args):
 
 
 def _arg_parsing():
-    parser = ArgumentParser(description="Take a screenshot")
+    parser = ArgumentParser(description=__doc__)
 
     # Mandatory arguments
-    parser.add_argument("url",
-                        help="url to screenshot")
+    parser.add_argument("url", help="url to screenshot")
 
-    parser.add_argument("path",
-                        help="path to image to be written")
+    parser.add_argument("path", help="path to image to be written")
 
     # Optionnal arguments
-    parser.add_argument("-f", "--fullpage",
-                        action="store_true",
+    parser.add_argument("-f", "--fullpage", action="store_true",
                         help="Take a screenshot of the whole scrollable page")
 
-    parser.add_argument("--width",
-                        type=int,
-                        metavar="width",
-                        default=800,
+    parser.add_argument("--width", type=int, metavar="width", default=800,
                         help="Set the width of the page")
 
-    parser.add_argument("--height",
-                        type=int,
-                        metavar="height",
-                        default=600,
+    parser.add_argument("--height", type=int, metavar="height", default=600,
                         help="Set the height of the page")
 
-    parser.add_argument("--wait_until",
-                        nargs="+",
-                        type=str,
+    parser.add_argument("--wait_until", nargs="+", type=str,
                         choices=["load", "domcontentloaded",
                                  "networkidle0", "networkidle2"],
                         help="How long do you want to wait for the page to be loaded")
 
     selector_group = parser.add_argument_group(title="CSS3 selectors (optional)",
-                                               description="Using quotes is recommended for complex CSS3 selectors")
-    selector_group.add_argument("--selector",
-                                metavar="seletor",
+                                               description="Using quotes is recommended for \
+                                                   complex CSS3 selectors")
+    selector_group.add_argument("--selector", metavar="seletor",
                                 help="The CSS3 selector of the element you want in the screenshot")
 
-    selector_group.add_argument("--wait_for",
-                                metavar="wait_for_selector",
-                                help="The CSS3 selector of an element you want to wait to be loaded before taking the screenshot")
+    selector_group.add_argument("--wait_for", metavar="wait_for_selector",
+                                help="The CSS3 selector of an element you want to wait to be \
+                                    loaded before taking the screenshot")
 
     return parser.parse_args()
 
