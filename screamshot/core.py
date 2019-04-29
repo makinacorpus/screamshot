6bceee0e42cccee037cb9e0479af9ef75acde1da
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-29 11:11:20 +0200

    Update core file serializer -> serialize deserializer -> deserialize

--- screamshot/core.py
+++ screamshot/core.py
@@ -2,12 +2,12 @@ from base64 import b64encode, b64decode
 from json import dumps, loads
 
 
-def serializer(img: bytes) -> str:
+def serialize(img: bytes, metadata: dict = None) -> str:
     b64_img = b64encode(img)
     str_img = b64_img.decode('utf-8')
     json_img = dumps({'image': str_img})
     return json_img
 
 
-def deserializer(data: str) -> bytes:
+def deserialize(data: str) -> bytes:
     json_img = loads(data)
