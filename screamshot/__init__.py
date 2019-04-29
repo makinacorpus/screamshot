470cdfadf1d5e50f0b2439637ce3a38e804a3830
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-29 15:42:40 +0200

    Update init file

--- screamshot/__init__.py
+++ screamshot/__init__.py
@@ -5,10 +5,11 @@
 
 
 __author__ = """Maxime Courtet & Félix Cloup"""
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 
 from screamshot.generate_bytes_img_functions import generate_bytes_img, generate_bytes_img_prom
+from screamshot.serializer import serialize, deserialize
 
 
-__all__ = ['generate_bytes_img', 'generate_bytes_img_prom']
+__all__ = ['generate_bytes_img', 'generate_bytes_img_prom', 'serialize', 'deserialize']
