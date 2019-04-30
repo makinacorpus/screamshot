72de92733d0d9f9a47c981dbd91f7565224eb657
Félix Cloup <felixcloup@gmail.com>
2019-04-30 17:45:20 +0200

    license changes

--- setup.py
+++ setup.py
@@ -13,43 +13,31 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 setup(
-    name='screamshot',
-
+    name="screamshot",
     version=screamshot.__version__,
-
     author=screamshot.__author__,
-
-    description='Python library to capture screenshots of web applications or pages',
-    
-    license='BSD',
-
-    long_description=open(os.path.join(HERE, 'README.md')).read() + '\n\n' +
-    open(os.path.join(HERE, 'CHANGES.md')).read(),
-
+    description="Python library to capture screenshots of web applications or pages",
+    license="License :: OSI Approved :: BSD License",
+    long_description=open(os.path.join(HERE, "README.md")).read()
+    + "\n\n"
+    + open(os.path.join(HERE, "CHANGES.md")).read(),
     include_package_data=True,
-    package_data={'screamshot': ['py.typed']},
-    packages=['screamshot'],
-
-    url='https://github.com/makinacorpus/screamshot',
-
+    package_data={"screamshot": ["py.typed"]},
+    packages=["screamshot"],
+    url="https://github.com/makinacorpus/screamshot",
     classifiers=[
-        'Programming Language :: Python',
-        'License :: BSD 2-Clause License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Screenshot',
+        "Programming Language :: Python",
+        "License :: BSD 2-Clause License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Screenshot",
     ],
-
-    install_requires=[
-        'pyppeteer',
-        'requests',
-    ],
-
+    install_requires=["pyppeteer", "requests"],
     entry_points={
-        'console_scripts': [
-            'screamshot = screamshot.screamshot_script:main',
-            'browser-manager = screamshot.browser_manager_script:main',
-        ],
+        "console_scripts": [
+            "screamshot = screamshot.screamshot_script:main",
+            "browser-manager = screamshot.browser_manager_script:main",
+        ]
     },
 )
