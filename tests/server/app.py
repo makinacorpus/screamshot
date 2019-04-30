0f5cde490ee8b754d59f317e33e97601c7573553
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-04-30 14:40:12 +0200

    Update app file Add a protected route

--- tests/server/app.py
+++ tests/server/app.py
@@ -1,13 +1,24 @@
 import logging
+from functools import wraps
 
-from flask import Flask, render_template, request, send_from_directory, url_for, redirect
+from flask import Flask, render_template, request, send_from_directory, url_for, redirect, jsonify, make_response
+from flask.wrappers import Response
 
+from jwt import encode, decode
 
-logger = logging.getLogger('werkzeug')
-logger.setLevel(logging.ERROR)
+# logger = logging.getLogger('werkzeug')
+# logger.setLevel(logging.ERROR)
 
 
 app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = '8OpElGqFyUritA-IQPYg8jMYkUHjRSjBmtkLirWsktg'
+app.config['TOKEN'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibWFraW5hIn0.jUTxi6c2-o3nHJ6Bq7zRXFoKixUyYetgPX3cToOayiA'
+
+
+class OwnResponse(Response):
+    def __init__(self, response=None, status=None, headers=None, mimetype=None,
+                 content_type=None, direct_passthrough=False):
+        super().__init__(response, status, {'token': app.config['TOKEN']}, mimetype, content_type, direct_passthrough)
 
 
 @app.route('/')
@@ -16,7 +27,7 @@ def index():
 
 
 @app.route('/other')
-def otherPage():
+def other_page():
     return redirect(url_for('static', filename='other.html'))
 
 
@@ -33,5 +44,28 @@ def close():
     return 'Server shutting down...'
 
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('token')
+        auth = request.authorization
+        if not token and (not auth or auth.username != 'makina' or auth.password != 'makina'):
+            return make_response('Missing token', 401,
+                                 {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        if not auth:
+            try:
+                data = decode(token, app.config['SECRET_KEY'])
+            except:
+                return make_response('Invalid token', 403)
+        return f(*args, **kwargs)
+    return decorated
+
+
+@app.route('/protected_other')
+@token_required
+def protected_other_page():
+    return redirect(url_for('static', filename='other.html'))
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
