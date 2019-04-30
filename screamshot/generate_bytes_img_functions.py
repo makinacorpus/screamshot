d18800a93cff40b6e24f811aa4901c4d1a65d60e
courtem <49525399+courtem@users.noreply.github.com>
2019-04-30 15:10:17 +0200

    Update generate_img_bytes file
    Update doc

--- screamshot/generate_bytes_img_functions.py
+++ screamshot/generate_bytes_img_functions.py
@@ -101,7 +101,7 @@ async def generate_bytes_img(url: str, **kwargs) -> bytes:
     :param credentials: If the website uses "login authentication", you can specify two fields: \
         `username` and `password`. Otherwise, if it uses a "token identification" that must be \
             specified in the header, please indicate into `credentials` the field that should be \
-                passed to the header, as well as a `token_in_header` field equal to `on`.
+                passed to the header, as well as a `token_in_header` field equal to `True`.
     :type credentials: dict
 
     :param width: optionnal, the window's width
@@ -176,7 +176,7 @@ async def generate_bytes_img_prom(url: str, future: Future, **kwargs):
     :param credentials: If the website uses "login authentication", you can specify two fields: \
         `username` and `password`. Otherwise, if it uses a "token identification" that must be \
             specified in the header, please indicate into `credentials` the field that should be \
-                passed to the header, as well as a `token_in_header` field equal to `on`.
+                passed to the header, as well as a `token_in_header` field equal to `True`.
     :type credentials: dict
 
     :param width: optionnal, the window's width
