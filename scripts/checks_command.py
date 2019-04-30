95e3b1e692f37de48a5f7af31e14e2ec06f53e65
courtem <49525399+courtem@users.noreply.github.com>
2019-04-30 17:10:24 +0200

    Update checks_command file
    Remove useless functions

--- scripts/checks_command.py
+++ scripts/checks_command.py
@@ -13,31 +13,19 @@ from screamshot.utils import wait_server_start, wait_server_close
 
 
 logger = getLogger()
-logger.setLevel(INFO)
+logger.setLevel(INFO)   
 
 
-def _parse_arg():
+def main():
     parser = ArgumentParser(description=__doc__)
-
     parser.add_argument('--wait-url',
                         help='The URL to wait before starting the tests and checks',
                         type=str, default='http://127.0.0.1:5000/index.html')
     parser.add_argument('--close-url',
                         help='The URL that shutdown the server',
                         type=str, default='http://127.0.0.1:5000/close')
-    return parser.parse_args()
+    args = parser.parse_args()
 
-
-def _parse_unittest_stdout(returncode, o_stdout):
-    if returncode == 1:
-        stdout = search(r'(?P<message>[^<]+FAILED)', o_stdout).group('message')
-    else:
-        stdout = search(r'(?P<message>[^<]+OK)', o_stdout).group('message')
-    return stdout
-
-
-def main():
-    args = _parse_arg()
     exit_code = 0
 
     logger.info('\n####################\n     WAIT SERVER     \n####################\n')
