02022c70687bd631fc1c101ecf0445f295a4a0d4
Maxime Courtet <maxime.courtet@makina-corpus.com>
2019-05-06 10:31:35 +0200

    Update init Remove serializer functions

--- screamshot/__init__.py
+++ screamshot/__init__.py
@@ -13,13 +13,10 @@ from screamshot.generate_bytes_img_functions import (
     generate_bytes_img_prom,
     generate_bytes_img_django_wrap,
 )
-from screamshot.serializer import serialize, deserialize
 
 
 __all__ = [
     "generate_bytes_img",
     "generate_bytes_img_prom",
     "generate_bytes_img_django_wrap",
-    "serialize",
-    "deserialize",
 ]
